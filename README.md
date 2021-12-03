9b1d2badb285d3fac9cab19ea9e716ca34318829
Wes Morgan <git@wesmorgan.me>
2021-12-03 07:59:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# pod-fluree-crypto
+
+This is a [Babashka]() [pod]() for the [fluree.crypto library]()
+
+## Building
+
+Pods are built as native binaries for their respective platforms, so this uses
+GraalVM native-image. You need to install a Graal JDK and the native-image tool
+and then run in this repo:
+
+`clojure -M:native-image`
+
+This should produce a binary named `pod-fluree-crypto` for your platform in the
+project root.
+
+## Usage
+
+```clojure
+(require '[babashka.pods :as pods])
+(pods/load-pod "path/to/pod-fluree-crypto")
+(require '[pod.fluree.crypto :as crypto])
+
+(crypto/sha2-256 "foo") ; => "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+```
+
+## License
+
+/*********************************************************************
+* Copyright (c) 2021 Fluree, PBC
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+  **********************************************************************/
---
+++ src/pod/fluree/crypto_impl.clj
@@ -0,0 +1,111 @@
+(ns pod.fluree.crypto-impl
+  (:require [fluree.crypto]
+            [bencode.core :as bencode]
+            [clojure.edn :as edn]
+            [clojure.walk :as walk]
+            [clojure.string :as str])
+  (:import (java.io EOFException PushbackInputStream OutputStream))
+  (:gen-class))
+
+(set! *warn-on-reflection* true)
+
+(def debug? true)
+
+(defn debug [& msg]
+  (when debug?
+    (binding [*out* *err*]
+      (apply println msg))))
+
+(def stdin (PushbackInputStream. System/in))
+(def stdout System/out)
+
+(defn read-bencode [in]
+  (bencode/read-bencode in))
+
+(defn write-bencode [^OutputStream out v]
+  (bencode/write-bencode out v)
+  (.flush out))
+
+(defn bytes->string [^bytes bs]
+  (String. bs))
+
+(defn podify-namespace
+  ([ns] (podify-namespace ns *ns* nil))
+  ([ns ns-prefix] (podify-namespace ns ns-prefix nil))
+  ([ns ns-prefix pod-name]
+   (let [pod-ns (if pod-name
+                  [ns-prefix pod-name]
+                  [ns-prefix])
+         vars   (-> ns ns-publics keys)]
+     {:name (symbol (str/join "." pod-ns))
+      :vars (mapv (fn [v] {:name v}) vars)})))
+
+(def describe-map
+  (delay
+    (walk/postwalk
+      #(if (ident? %)
+         (name %)
+         %)
+      {:format     :edn
+       :namespaces [(podify-namespace 'fluree.crypto 'pod.fluree.crypto)]})))
+
+(defn invoke-fn [id message]
+  (let [var      (-> message
+                     (get "var")
+                     bytes->string
+                     symbol)
+        var-ns   (-> var namespace (str/split #"[.]") rest (->> (str/join "."))
+                     symbol) ; drop leading pod.
+        var-name (-> var name symbol)
+        _        (debug "invoke var:" (str var-ns "/" var-name))
+        f        (ns-resolve var-ns var-name)
+        _        (debug "resolved var:" (pr-str f))
+        args     (-> message (get "args") bytes->string
+                     edn/read-string)
+        _        (debug "args:" (pr-str args))
+        result   (apply f args)]
+    {"value"  (pr-str result)
+     "id"     id
+     "status" ["done"]}))
+
+(defn error-response [id throwable]
+  {"ex-message" (ex-message throwable)
+   "ex-data"    (pr-str
+                  (assoc (ex-data throwable)
+                    :type
+                    (str (class throwable))))
+   "id"         id
+   "status"     ["done" "error"]})
+
+(defn -main []
+  (loop []
+    (let [message (try (read-bencode stdin)
+                       (catch EOFException _
+                         ::EOF))]
+      (when-not (identical? ::EOF message)
+        (let [op (-> message (get "op") bytes->string keyword)
+              id (or (some-> message (get "id") bytes->string)
+                     "unknown")]
+          (case op
+            :describe (do
+                        (debug "got :describe")
+                        (debug "responding with" (pr-str @describe-map))
+                        (write-bencode stdout @describe-map)
+                        (recur))
+            :invoke (do
+                      (debug "got :invoke")
+                      (try
+                        (let [response (invoke-fn id message)]
+                          (debug "responding with" (pr-str @describe-map))
+                          (write-bencode stdout response))
+                        (catch Throwable e
+                          (->> e (error-response id) (write-bencode stdout))))
+                      (recur))
+            :shutdown (System/exit 0)
+            (do
+              (let [response {"ex-message" "Unknown op"
+                              "ex-data"    (pr-str {:op op})
+                              "id"         id
+                              "status"     ["done" "error"]}]
+                (write-bencode stdout response))
+              (recur))))))))
